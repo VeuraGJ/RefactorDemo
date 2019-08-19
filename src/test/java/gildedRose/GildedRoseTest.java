c07b053ffe817d24924759d7e46797804357569c
VeuraGJ <yuanxiao_ru@163.com>
2019-08-19 17:37:09 +0800

    feat:test case: should_return_9_given_a_normal_item_its_sellIn_is_8_quality_is_10

---
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -0,0 +1,22 @@
+package gildedRose;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GildedRoseTest {
+    @Test
+    public void should_return_9_given_a_normal_item_its_sellIn_is_8_quality_is_10(){
+        //given
+        Item[] items = {new Item("Noraml",8,10)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        //when
+        gildedRose.updateQuality();
+
+        //then
+        Assert.assertEquals(9,items[0].quality);
+    }
+
+}
