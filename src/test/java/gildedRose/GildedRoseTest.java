d4bb19ee3cd4b2bca4cd98c5dad1054ed383ff4e
VeuraGJ <yuanxiao_ru@163.com>
2019-08-19 17:49:16 +0800

    feat:test case:should_return_10_given_a_Sulfuras_item_its_sellIn_is_8_quality_is_10

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -31,6 +31,18 @@ public class GildedRoseTest {
         //then
         Assert.assertEquals(0,items[0].quality);
     }
+    @Test
+    public void should_return_10_given_a_Sulfuras_item_its_sellIn_is_8_quality_is_10(){
+        //given
+        Item[] items = {new Item("Sulfuras, Hand of Ragnaros",8,10)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        //when
+        gildedRose.updateQuality();
+
+        //then
+        Assert.assertEquals(10,items[0].quality);
+    }
 
 
 }
